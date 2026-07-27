41fd37d4810ae0379d43f513833d08b181eaa75f
AKSHAYA C S <akshayacs2008@gmail.com>
2026-07-28 00:09:53 +0530

    Delete Javascript directory

--- Javascript/dom2.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello world");
-
-// document.body.firstElementChild.childNodes
-// NodeList(11) [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
-// document.body.firstElementChild.children
-// HTMLCollection(5) [div.box, div.box, div.box, div.box, div.box]
--- Javascript/new.js
+++
@@ -1,3 +0,0 @@
-console.log("Hello World!!");
-console.log("Hey I am Console");
-console.log("Code is running");
