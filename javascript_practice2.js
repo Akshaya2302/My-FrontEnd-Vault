3cd396f8ed875df9193a0c6b28a762621d8bf243
AKSHAYA C S <akshayacs2008@gmail.com>
2026-07-31 23:38:33 +0530

    Javascript

---
+++ javascript_practice2.js
@@ -0,0 +1,43 @@
+console.log("Hey this is the practice 2 of javascript..");
+
+let a = 5;
+// a = a + 1;
+let b = 6;
+let c = "akshu";
+
+console.log(a + b + 8);
+// this typeof is used to know the type of the variables
+console.log(typeof a, typeof b, typeof c);
+{
+  let a = 66;
+  console.log(a);
+}
+console.log(a);
+
+const a1 = 6;
+// a1 = a1 + 1;
+// const in js means we can't change the values of it coz it is constant value ex, pi value 3.14
+
+// Dat types in javascript
+let x = "Akshu";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+// object is key value combination in js
+let o = {
+  name: "akshu",
+  "job code": 5600,
+  "is preety": true,
+};
+
+console.log(o);
+o.salary = "1000000";
+console.log(o);
+o.salary = "2000000";
+console.log(o);
