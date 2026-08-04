dcad4beaf2ea7f30dd950e487d657268c1831dac
AKSHAYA C S <akshayacs2008@gmail.com>
2026-08-04 21:49:04 +0530

    dOM

---
+++ dom2.js
@@ -0,0 +1,6 @@
+console.log("Hello world");
+
+// document.body.firstElementChild.childNodes
+// NodeList(11) [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
+// document.body.firstElementChild.children
+// HTMLCollection(5) [div.box, div.box, div.box, div.box, div.box]
